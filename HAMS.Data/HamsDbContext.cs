3e25ced81a2487d5de9bbe14cbe49efebb16f02c
Vishal Yadav <vishaljyadav576@gmail.com>
2025-07-13 12:04:54 +0530

    chore: initial commit

---
+++ HAMS.Data/HamsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HAMS.Data
+{
+    public class HamsDbContext : DbContext 
+    {
+
+    }
+}
